8bca1831fce9d5a9f3b720c5163f33164ac72e10
NitramLegov <Martin_Vogel@live.com>
2018-10-13 15:52:55 +0200

    Update Install_Mosquitto.sh

--- Install_Mosquitto.sh
+++ Install_Mosquitto.sh
@@ -4,3 +4,4 @@ echo 'Welcome to our mosquitto installation script.'
 echo 'Updating the apt-get index'
 sudo apt-get -qq update >> /dev/null
 sudo apt-get -qq install mosquitto
+sudo apt-get -qq install mosquitto-clients
